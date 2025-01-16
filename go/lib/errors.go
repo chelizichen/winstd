36a0762b43d6421e22ace6abd0455e1edb2a9df3
chelizichen <1347290221@qq.com>
2025-01-17 00:38:49 +0800

    errors

--- go/lib/errors.go
+++ go/lib/errors.go
@@ -41,3 +41,23 @@ func ThrowERROR_CODE_THREE_PART(uid, msg string) *T_Error {
 func ThrowERROR_CODE_DB_ERROR(uid, msg string) *T_Error {
 	return NewT_Error(uid, msg, CODE_DB_ERROR)
 }
+
+// test
+
+func TEST_errorHandler(retryNum int) (bool, *T_Error) {
+	if retryNum >= 2 {
+		return true, nil
+	}
+	return false, ThrowERROR_CODE_DB_ERROR(fmt.Sprint(retryNum), "数据库写入异常")
+}
+
+func TEST_serviceFunc(retryNum int) {
+	isNext, err := TEST_errorHandler(retryNum)
+	if err != nil && err.GetCode() == CODE_DB_ERROR {
+		fmt.Printf("执行失败，进入重试 %v \n", retryNum)
+		err.PrintStackError()
+		TEST_serviceFunc(retryNum + 1)
+		return
+	}
+	fmt.Println("执行成功", isNext)
+}
--- go/main.go
+++ go/main.go
@@ -1,20 +1,5 @@
 package main
 
-import (
-	"fmt"
-
-	"chelizichen.com/win/lib"
-)
-
 func main() {
-	fmt.Println("hello world")
-	isNext, err := errorHandler()
-	if err != nil && err.GetCode() == lib.CODE_DB_ERROR {
-		fmt.Println("handle db error")
-	}
-	fmt.Println("isNext", isNext)
-}
-
-func errorHandler() (bool, *lib.T_Error) {
-	return false, lib.ThrowERROR_CODE_DB_ERROR("1234", "asmndkoasd")
+	// lib.TEST_serviceFunc(0)
 }
